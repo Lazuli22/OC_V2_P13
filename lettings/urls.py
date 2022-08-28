de17688702e8096329e89acae21c72fbfa67ae35
Dolores DIAZ <diaz.dolores@gmail.com>
2022-08-28 15:18:58 +0200

    deplacement des urls et vues lettings

--- lettings/urls.py
+++ lettings/urls.py
@@ -1,6 +1,7 @@
+from django.urls import path
+from . import views
 
-
-
-path('lettings/', views.lettings_index, name='lettings_index'),
-path('lettings/<int:letting_id>/', views.letting, name='letting'),
-    
+urlpatterns = [
+    path('', views.index, name='lettings_index'),
+    path('<int:letting_id>/', views.letting, name='letting'),
+]
