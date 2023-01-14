82234a7791a012041b5e5f019314dc87dbd18ed8
Dolores DIAZ <diaz.dolores@gmail.com>
2023-01-14 23:31:18 +0100

    ajout whitenoise

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -123,7 +123,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-django_heroku.settings(locals())
 
 sentry_sdk.init(
     dsn=env('DNS'),
